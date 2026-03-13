ec650bf2639c5c09f778074828e95ec0c1934307
RajantiSubangkit <34586431+RajantiSubangkit@users.noreply.github.com>
2026-03-13 14:02:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -303,10 +303,9 @@ if uploaded_video and st.session_state.running:
         if x is not None:
             cv2.circle(frame,(x,y),6,(0,0,255),-1)
             cv2.circle(neg_frame,(x,y),6,(255,0,0),-1)
-
-        if frame_id % 3 == 0:
-            raw_video.image(frame, channels="BGR")
-            neg_video.image(neg_frame, channels="BGR")
+            
+        raw_video.image(frame, channels="BGR")
+        neg_video.image(neg_frame, channels="BGR")
 
         track=pd.DataFrame({"X":X,"Y":Y})
 
