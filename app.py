707a096505de8d00bd042cab350138193d742ebf
RajantiSubangkit <34586431+RajantiSubangkit@users.noreply.github.com>
2026-03-13 08:39:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -29,18 +29,12 @@ uploaded_video = st.file_uploader("Upload your video")
 # reset ROI if new video uploaded
 if uploaded_video is not None:
 
-    if "last_video" not in st.session_state:
-        st.session_state.last_video = uploaded_video.name
+    if uploaded_video is not None:
 
-    if uploaded_video.name != st.session_state.last_video:
-
-        st.session_state.last_video = uploaded_video.name
-
-        if "roi" in st.session_state:
-            del st.session_state["roi"]
-
-        if "roi_points" in st.session_state:
-            del st.session_state["roi_points"]
+    # reset ROI every time a new file is uploaded
+    for key in ["roi", "roi_points", "video_path"]:
+        if key in st.session_state:
+            del st.session_state[key]
 #####selector
 # ROI selection
 roi = None
